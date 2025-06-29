d952d5a131a78c6ceca684cab328c32bfcd79d98
BrunoEspindola <brunoespindola.work@gmail.com>
2025-06-29 19:26:33 -0300

    feat: update logs

--- logs/src/server.js
+++ logs/src/server.js
@@ -6,11 +6,12 @@ const bodyParser = require('body-parser');
 const RabbitMQConsumer = require('@services/RabbitMQConsumer');
 
 const app = express();
-const port = 5001;
+const port = 5002;
 
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 
 app.listen(port, () => {
+  console.log(`Server running on the port: ${port}`);
   RabbitMQConsumer.listenChat();
 });
